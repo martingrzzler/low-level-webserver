f8d1d1d01114d79603a55fa78115ed1c9079b805
martingrzzler <martin.gressler@code.berlin>
2020-09-12 19:16:05 +0200

    created Shinkansen class

---
+++ src/Shinkansen.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <iostream>
+#include "Utils.h"
+
+using namespace std;
+
+#define PORT 8080
+
+class Shinkansen
+{
+private:
+  /* data */
+public:
+  Shinkansen(/* args */);
+  ~Shinkansen();
+};
